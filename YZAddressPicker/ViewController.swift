b0c401d98a299f0bf18062ff5e8025d116c4cba9
Shin1122 <big_shin@126.com>
2017-02-02 14:17:50 +0800

    仿美团地址选择页swift版

---
+++ YZAddressPicker/ViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  YZAddressPicker
+//
+//  Created by Shin on 2017/1/31.
+//  Copyright © 2017年 YZ.Shin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //1.定义一个按钮,给定尺寸
+        let btn = UIButton(frame: CGRect(x: self.view.frame.size.width/2-100 , y:self.view.frame.size.height/2-20 , width: 200, height: 40))
+        btn.setTitle("请选择地址", for:.normal)
+        btn.setTitleColor(UIColor.black, for: .normal)
+        self.view.addSubview(btn)
+        
+        //2.添加点击事件
+        btn.addTarget(self, action: #selector(changeCitys) , for: .touchUpInside)
+        
+        
+        
+        
+        
+    }
+    
+    //修改该按钮的title变化方法
+    func changeCitys() {
+        
+        let controller = YZCityViewController()
+        self.present(controller, animated: true) {
+            
+        }
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ YZAddressPicker/YZCityViewController.swift
@@ -0,0 +1,52 @@
+//
+//  YZCityViewController.swift
+//  YZAddressPicker
+//
+//  Created by Shin on 2017/1/31.
+//  Copyright © 2017年 YZ.Shin. All rights reserved.
+//
+
+import UIKit
+
+class YZCityViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        //加工数据
+        
+        
+        
+        //视图布局
+        
+        
+        
+    }
+
+    //加工城市数据方法
+    func makeCitys() {
+        
+    }
+    
+    
+    
+    
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
